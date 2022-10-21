6b7f109d208fa7d07f9ac76e5f711979bcca64ea
Chenkai Yao <yck2000@bu.edu>
2022-10-21 12:32:45 -0400

    o_MA

---
+++ load_and_clean_data.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+od_data <- read_csv(here::here("dataset-ignore", "od.csv"))
+
+## CLEAN the data
+o_MA <- data %>% 
+  filter(o_state_name == "Massachusetts") %>%
+  separate(pool,
+           into = c("race","income"),
+           sep = "Q"
+  ) 
+
+o_MA[is.na(o_MA)] = 0
+
+
+write_csv(o_MA, file = here::here("dataset", "o_MA.csv"))
+
+save(o_MA, file = here::here("dataset/o_MA.RData"))
