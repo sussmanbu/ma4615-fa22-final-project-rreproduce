0442b5576d3aa7d86b447d4e8b29fb1030f2defc
Chenkai Yao <yck2000@bu.edu>
2022-10-21 15:56:30 -0400

    3:36pm

--- load_and_clean_data.R
+++ load_and_clean_data.R
@@ -7,10 +7,14 @@ o_MA <- data %>%
   filter(o_state_name == "Massachusetts") %>%
   separate(pool,
            into = c("race","income"),
-           sep = "Q"
-  ) 
+           sep = -2
+  ) %>%
+  filter(n != 0 & n != -1) 
 
 o_MA[is.na(o_MA)] = 0
+
+colnames(o_MA) <- c('Num_original_CZ','Original_CZ','Original_State','Num_dest_CZ','Dest_CZ','Dest_state','n','From_origin','Live_in_dest','race','income','pr_d_o','pr_o_d')
+
 
 write_csv(o_MA, file = here::here("dataset-ignore", "o_MA.csv"))
 
