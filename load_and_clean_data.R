0b38a5aa1a94780223f4ec318f2e2772c03eb7e7
Chenkai Yao <yck2000@bu.edu>
2022-10-21 19:57:09 -0400

    7:56

--- load_and_clean_data.R
+++ load_and_clean_data.R
@@ -16,6 +16,6 @@ o_MA[is.na(o_MA)] = 0
 colnames(o_MA) <- c('Num_original_CZ','Original_CZ','Original_State','Num_dest_CZ','Dest_CZ','Dest_state','n','From_origin','Live_in_dest','race','income','pr_d_o','pr_o_d')
 
 
-write_csv(o_MA, file = here::here("dataset-ignore", "o_MA.csv"))
+write_csv(o_MA, file = here::here("dataset", "o_MA.csv"))
 
-save(o_MA, file = here::here("dataset-ignore/o_MA.RData"))
+save(o_MA, file = here::here("dataset/o_MA.RData"))
